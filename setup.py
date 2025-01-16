1b8ef8ea775e72367a453068aed0d62270b93792
Ella Dwipujana Asyani <elladwipujana1103@gmail.com>
2025-01-16 17:06:21 +0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name                = 'Chatbot Website',
+    version             = '0.0.0',
+    author              = 'Ella Dwipujana Asyani',
+    author_email        = 'elladwipujana1103@gmail.com',
+    packages            = find_packages(),
+    install_requires    = []
+
+)
